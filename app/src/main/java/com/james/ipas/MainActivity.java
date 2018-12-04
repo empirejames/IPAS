cc4e5876f65428babdb40aa0cfafbdf5e1707a29
JamesChu <empirejames@hotmail.com>
2018-12-04 18:24:38 +0800

    Adding multi subject

--- app/src/main/java/com/james/ipas/MainActivity.java
+++ app/src/main/java/com/james/ipas/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
 import android.util.Log;
 import android.view.View;
@@ -15,6 +16,8 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -24,16 +27,18 @@ import com.google.firebase.database.ValueEventListener;
 import java.util.ArrayList;
 import java.util.Random;
 
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
 import com.weiwangcn.betterspinner.library.material.MaterialBetterSpinner;
 
 public class MainActivity extends Activity {
 
-    TextView next_grade, grade, tv_spendTime, tv_spend ;
+    TextView next_grade, grade, tv_spendTime, tv_spend;
     ImageView img_ipas;
     Button btn_enter;
     Spinner spinner, spiner_subject;
     String getResult, spendTime;
     MaterialBetterSpinner subjectSpinner, countSpiner;
+    TinyDB tinyDB;
 
     @Override
     protected void onCreate(Bundle savedInstancesState) {
@@ -41,6 +46,9 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         getWindow().setBackgroundDrawableResource(R.drawable.ipas);
         initView();
+
+        //new GetRemoteConfig().execute();
+        tinyDB = new TinyDB(this);
         Bundle b = getIntent().getExtras();
         if (b != null) {
             getResult = b.getString("result");
@@ -59,14 +67,22 @@ public class MainActivity extends Activity {
 
                 String txt_question = countSpiner.getText().toString();
                 String txt_subject = subjectSpinner.getText().toString();
-                if(txt_subject.equals("") || txt_question.equals("")){
+                if (txt_subject.equals("行動裝置概論")) {
+                    txt_subject = "mobile_intruduction";
+                } else if (txt_subject.equals("Android程式設計")) {
+                    txt_subject = "mobile_android";
+                } else if (txt_subject.equals("iOS程式設計")) {
+                    txt_subject = "mobile_ios";
+                }
+
+                if (txt_subject.equals("") || txt_question.equals("")) {
                     //Toast.makeText(MainActivity.this, "請選擇科目題數 ", Toast.LENGTH_LONG).show();
                     View view = findViewById(R.id.next_grade);
                     Snackbar.make(view,
-                            "忘記選擇題庫 & 題數了喔～" ,
+                            "忘記選擇題庫 & 題數了喔～",
                             Snackbar.LENGTH_SHORT)
                             .show();
-                }else{
+                } else {
                     Intent i = new Intent(MainActivity.this, QuestionActivity.class);
                     Bundle b = new Bundle();
                     b.putString("subject", txt_subject);
@@ -76,15 +92,11 @@ public class MainActivity extends Activity {
                 }
             }
         });
-
-
-        ArrayList<String> passingData = new ArrayList<String>();
-
     }
 
     public void initView() {
         final String[] questions = {"10", "20", "30", "40", "50"};
-        final String[] subject = {"行動裝置概論"};
+        final String[] subject = {"行動裝置概論", "Android程式設計", "iOS程式設計"};
         img_ipas = (ImageView) findViewById(R.id.img_ipas);
 
         next_grade = findViewById(R.id.next_grade);
@@ -105,6 +117,55 @@ public class MainActivity extends Activity {
 
     }
 
+    public void getRemotePara() {
+        final FirebaseRemoteConfig mRemoteConfig = FirebaseRemoteConfig.getInstance();
+        long cacheExpiration = 3600;
+        if (mRemoteConfig.getInfo().getConfigSettings().isDeveloperModeEnabled()) {
+            cacheExpiration = 0;
+        }
+        mRemoteConfig.fetch(cacheExpiration)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            mRemoteConfig.activateFetched();
+
+                            String introduction = mRemoteConfig.getString("mobile_introduction_num");
+                            tinyDB.putString("mobile_introduction_num", introduction);
+
+                            String android = mRemoteConfig.getString("mobile_android_num");
+                            tinyDB.putString("mobile_android_num", android);
+
+                            String ios = mRemoteConfig.getString("mobile_ios_num");
+                            tinyDB.putString("mobile_android_num", ios);
+
+                            String result = mRemoteConfig.getString("default");
+                            tinyDB.putString("default", result);
+
+                            Log.e("GGG", introduction);
+                            Log.e("GGG", android);
+                            Log.e("GGG", ios);
+                            Log.e("GGG", result);
+
+                        } else {
+                            Log.e("GGG", "Un success");
+                        }
+                    }
+                });
+    }
+
+    class GetRemoteConfig extends AsyncTask<int[], Integer, String> {
+        @Override
+        protected String doInBackground(int[]... ints) {
+            getRemotePara();
+            return "finish";
+        }
+
+        @Override
+        protected void onPreExecute() {
+
+        }
+    }
 
 }
 
--- app/src/main/java/com/james/ipas/QuestionActivity.java
+++ app/src/main/java/com/james/ipas/QuestionActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.annotation.NonNull;
 import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
 import android.view.View;
@@ -13,25 +14,26 @@ import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
 
 import java.util.ArrayList;
 import java.util.Random;
 
-public class QuestionActivity extends Activity implements View.OnClickListener{
+public class QuestionActivity extends Activity implements View.OnClickListener {
     String TAG = QuestionActivity.class.getSimpleName();
-    int numberOfSelect = 3;
-    int max_number = 250;
+    int numberOfSelect ;
+    int max_number ;
     int[] randItem = new int[numberOfSelect];
     ArrayList<TestItem> testLists = new ArrayList<TestItem>();
-    TextView tv_selector, tv_content , txt_getCount;
-    RadioGroup radioGroup;
-    RadioButton radioButton_A, radioButton_B, radioButton_C, radioButton_D;
-    Button btn_next, btn_pre , btn_A, btn_B, btn_C, btn_D;
+    TextView tv_selector, tv_content, txt_getCount;
+    Button btn_next, btn_pre, btn_A, btn_B, btn_C, btn_D;
     int page = 0;
     int getAnswer;
     int answer = 0;
@@ -40,16 +42,18 @@ public class QuestionActivity extends Activity implements View.OnClickListener{
     private Long startTime;
     String spendTimes;
     private Handler handler = new Handler();
-
+    String subject, question;
+    TinyDB tinyDB ;
 
     @Override
     protected void onCreate(Bundle savedInstancesState) {
         super.onCreate(savedInstancesState);
         setContentView(R.layout.activity_question);
         Bundle bundle = getIntent().getExtras();
-        String subject = bundle.getString("subject");
-        String question = bundle.getString("question");
+        subject = bundle.getString("subject");
+        question = bundle.getString("question");
         numberOfSelect = Integer.parseInt(question);
+        tinyDB = new TinyDB(this);
         initView();
         //Log.e(TAG, "subject : " + subject);
         //Log.e(TAG, "question : " + question);
@@ -66,7 +70,20 @@ public class QuestionActivity extends Activity implements View.OnClickListener{
         //設定Delay的時間
         handler.postDelayed(updateTimer, 1000);
 
-
+        if(subject.equals("mobile_intruduction")){
+            max_number = 250;
+            //max_number = Integer.parseInt(tinyDB.getString("mobile_intruduction"));
+        }else if(subject.equals("mobile_android")){
+            max_number = 50;
+            //max_number = Integer.parseInt(tinyDB.getString("mobile_android"));
+        }else if(subject.equals("mobile_ios")){
+            max_number = 50;
+            //max_number = Integer.parseInt(tinyDB.getString("mobile_ios"));
+        }else{
+            max_number = 10;
+            //max_number = Integer.parseInt(tinyDB.getString("default"));
+        }
+        Log.e(TAG,"Get Remote Data : max_number  - " + max_number);
         randItem = genRandNum(max_number, numberOfSelect);
         new GetData().execute(randItem);
         btn_next.setOnClickListener(new View.OnClickListener() {
@@ -74,7 +91,7 @@ public class QuestionActivity extends Activity implements View.OnClickListener{
             public void onClick(View v) {
                 if (page < numberOfSelect - 1) {
                     if (getAnswer != answer) {
-                        Log.e(TAG, "正確解答 : " + answer +"  , 您的解答:  " + getAnswer);
+                        Log.e(TAG, "正確解答 : " + answer + "  , 您的解答:  " + getAnswer);
 
                         Log.e(TAG, "showAnswer : " + showFlag);
                         showFlag = showAnswer(answer);
@@ -108,7 +125,7 @@ public class QuestionActivity extends Activity implements View.OnClickListener{
     }
 
     public boolean showAnswer(int answer) {
-        countCorrect ++;
+        countCorrect++;
         if (answer == 1) {
             btn_A.setTextColor(getResources().getColor(R.color.red));
         } else if (answer == 2) {
@@ -122,14 +139,14 @@ public class QuestionActivity extends Activity implements View.OnClickListener{
     }
 
 
-    public void resetBtnBackground(){
+    public void resetBtnBackground() {
         btn_A.setBackgroundResource(R.drawable.rule_button_shape);
         btn_B.setBackgroundResource(R.drawable.rule_button_shape);
         btn_C.setBackgroundResource(R.drawable.rule_button_shape);
         btn_D.setBackgroundResource(R.drawable.rule_button_shape);
     }
 
-    public void resetBtnTextColor(){
+    public void resetBtnTextColor() {
         btn_A.setTextColor(getResources().getColor(R.color.colorWhite));
         btn_B.setTextColor(getResources().getColor(R.color.colorWhite));
         btn_C.setTextColor(getResources().getColor(R.color.colorWhite));
@@ -140,20 +157,22 @@ public class QuestionActivity extends Activity implements View.OnClickListener{
 
         resetBtnTextColor();
         resetBtnBackground();
-        txt_getCount.setText(page+1 + " / " + numberOfSelect);
+        txt_getCount.setText(page + 1 + " / " + numberOfSelect);
 
         if (testLists.size() != 0 && page >= 0) {
-            Log.e(TAG,"testLists size" + "  :  " +testLists.size() + " Page  :" + page);
+            Log.e(TAG, "testLists size" + "  :  " + testLists.size() + " Page  :" + page);
+
+
             tv_content.setText(testLists.get(page).getTopic());
             tv_content.setMovementMethod(ScrollingMovementMethod.getInstance());
             btn_A.setText(testLists.get(page).getItem_A());
             btn_B.setText(testLists.get(page).getItem_B());
             btn_C.setText(testLists.get(page).getItem_C());
             btn_D.setText(testLists.get(page).getItem_D());
-            Log.e(TAG,"Update Page" + "    " + testLists.get(page).getTopic());
-            Log.e(TAG,"Update Page" + "    " + testLists.get(page).getAnswer());
-            Log.e(TAG,"Update Page" + "    " + testLists.get(page).getItem_A());
-            Log.e(TAG,"Update Page" + "    " + testLists.get(page).getItem_B());
+            Log.e(TAG, "Update Page" + "    " + testLists.get(page).getTopic());
+            Log.e(TAG, "Update Page" + "    " + testLists.get(page).getAnswer());
+            Log.e(TAG, "Update Page" + "    " + testLists.get(page).getItem_A());
+            Log.e(TAG, "Update Page" + "    " + testLists.get(page).getItem_B());
 
 
             if (testLists.get(page).getAnswer().contains("A")) {
@@ -184,13 +203,14 @@ public class QuestionActivity extends Activity implements View.OnClickListener{
 
         Bundle b = new Bundle();
         int result = numberOfSelect - countCorrect;
-        b.putString("result" , result + " / " + numberOfSelect);
-        b.putString("spendTimes" , spendTimes);
+        b.putString("result", result + " / " + numberOfSelect);
+        b.putString("spendTimes", spendTimes);
         Log.e(TAG, " Go Home Page : " + result);
         Intent i = new Intent(QuestionActivity.this, MainActivity.class);
         i.putExtras(b);
         startActivity(i);
     }
+
     private Runnable updateTimer = new Runnable() {
         public void run() {
             final TextView time = (TextView) findViewById(R.id.timer);
@@ -198,25 +218,24 @@ public class QuestionActivity extends Activity implements View.OnClickListener{
 
             Long spentTime = System.currentTimeMillis() - startTime;
             //計算目前已過分鐘數
-            Long minius = (spentTime/1000)/60;
+            Long minius = (spentTime / 1000) / 60;
             //計算目前已過秒數
-            Long seconds = (spentTime/1000) % 60;
+            Long seconds = (spentTime / 1000) % 60;
 
             txt_minius = String.valueOf(minius);
             txt_seconds = String.valueOf(seconds);
 
-            if(txt_minius.length()<2){
-                txt_minius = "0"+txt_minius;
+            if (txt_minius.length() < 2) {
+                txt_minius = "0" + txt_minius;
             }
-            if(txt_seconds.length()<2){
-                txt_seconds = "0"+txt_seconds;
+            if (txt_seconds.length() < 2) {
+                txt_seconds = "0" + txt_seconds;
             }
-            time.setText(txt_minius+" : "+txt_seconds);
-            spendTimes = txt_minius+" : "+txt_seconds;
+            time.setText(txt_minius + " : " + txt_seconds);
+            spendTimes = txt_minius + " : " + txt_seconds;
             handler.postDelayed(this, 1000);
         }
     };
-
 
 
     public int[] genRandNum(int max_number, int numberOfSelect) {
@@ -237,18 +256,18 @@ public class QuestionActivity extends Activity implements View.OnClickListener{
     @Override
     public void onClick(View v) {
         resetBtnBackground();
-        switch (v.getId()){
-            case  R.id.btn_A :
+        switch (v.getId()) {
+            case R.id.btn_A:
                 getAnswer = 1;
                 btn_A.setBackgroundResource(R.drawable.rule_error_button_shape);
                 resetBtnTextColor();
                 break;
-            case R.id.btn_B :
+            case R.id.btn_B:
                 getAnswer = 2;
                 btn_B.setBackgroundResource(R.drawable.rule_error_button_shape);
                 resetBtnTextColor();
                 break;
-            case   R.id.btn_C:
+            case R.id.btn_C:
                 getAnswer = 3;
                 btn_C.setBackgroundResource(R.drawable.rule_error_button_shape);
                 resetBtnTextColor();
@@ -274,19 +293,16 @@ public class QuestionActivity extends Activity implements View.OnClickListener{
 
         @Override
         protected String doInBackground(final int[]... strings) {
-            DatabaseReference ref = FirebaseDatabase.getInstance().getReference();
+            Log.e(TAG,  "Get subject : " + subject);
+            DatabaseReference ref = FirebaseDatabase.getInstance().getReference(subject);
             ref.addListenerForSingleValueEvent(new ValueEventListener() {
                 @Override
                 public void onDataChange(DataSnapshot dataSnapshot) {
                     int[] randResult = strings[0];
-
-                    for (DataSnapshot ds : dataSnapshot.getChildren()) {
-
-                        for (int i = 0; i < randResult.length; i++) {
-                            TestItem items = ds.child(randResult[i] + "").getValue(TestItem.class);
-                            testLists.add(items);
-                            Log.e(TAG, "testList Size : " + testLists.size());
-                        }
+                    for (int i = 0; i < randResult.length; i++) {
+                        Log.e(TAG, dataSnapshot.getChildren() + " : Get Key"  );
+                        TestItem items = dataSnapshot.child(randResult[i] + "").getValue(TestItem.class);
+                        testLists.add(items);
                     }
                     updateView(page);
                 }
